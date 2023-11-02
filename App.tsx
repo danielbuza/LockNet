54c96fd9b8132e5d9530ba0fcad23f0009250e2f
georgy sam <georgysam@192.168.1.117>
2023-11-02 19:27:01 +0000

    “changes”

--- App.tsx
+++ App.tsx
@@ -1,56 +1,241 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Login from "./screens/Login";
-import Details from "./screens/Details";
-import { User, onAuthStateChanged } from "firebase/auth";
-import { useEffect, useState } from "react";
-import { FIREBASE_AUTH } from "./FirebaseConfig";
-import MyAccount from "./screens/MyAccount";
-import LandingPage from "./screens/LandingPage";
+import React, { useState } from 'react';
+import { View, TextInput, Button, Image, StyleSheet, TouchableOpacity,Text, ScrollView } from 'react-native';
 
-const Stack = createNativeStackNavigator();
+const AddContactScreen = () => {
+  const [imageSource, setImageSource] = useState(null);
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
 
-const InsideStack = createNativeStackNavigator();
+  const selectImage = () => {
+    // Add your code to select an image here.
+    // You can use libraries like react-native-image-picker or react-native-image-crop-picker.
+  };
 
-function InsideLayout() {
-  return (
-    <InsideStack.Navigator>
-      <InsideStack.Screen name="My Account" component={MyAccount} />
-      <InsideStack.Screen name="Details" component={Details} />
-    </InsideStack.Navigator>
-  );
-}
-
-export default function App() {
-  const [user, setUser] = useState<User | null>(null);
-  useEffect(() => {
-    onAuthStateChanged(FIREBASE_AUTH, (user) => {
-      console.log("user", user);
-      setUser(user);
+  const saveContact = () => {
+    // Here, you can save the contact data to your database or state.
+    // For simplicity, we'll just log the data for now.
+    console.log({
+      name,
+      surname,
+      phoneNumber,
+      imageSource,
     });
-  });
+  };
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName={user ? "Inside" : "LandingPage"}>
-        {user ? (
-          <Stack.Screen
-            name="Inside"
-            component={InsideLayout}
-            options={{ headerShown: false }}
-          />
-        ) : (
-          <Stack.Screen
-            name="LandingPage"
-            component={LandingPage}
-            options={{ headerShown: false }}
-          />
-        )}
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{ headerShown: false }}
+    <ScrollView contentContainerStyle={styles.container}>
+   <View style={styles.container}>
+   <TouchableOpacity style={styles.circle} onPress={() => console.log('Image pressed')}>
+      <Image
+        source={require('./assets/Vector_add_photo.png')} // Provide the path to your image
+        style={styles.imageIcon}
+      />
+    </TouchableOpacity>
+        
+        <View style={styles.SummaryBox}>
+        <TextInput
+          placeholder="Write a short summary about this person"
+          value={name}
+          onChangeText={(text) => setName(text)}
         />
-      </Stack.Navigator>
-    </NavigationContainer>
+      </View>
+
+      <View style={styles.inputBox}>
+        <TextInput
+          placeholder="Name"
+          value={name}
+          onChangeText={(text) => setName(text)}
+        />
+      </View>
+
+      <View style={styles.inputBox}>
+        <TextInput
+          placeholder="Surname"
+          value={surname}
+          onChangeText={(text) => setSurname(text)}
+        />
+      </View>
+
+      <View style={styles.inputBox}>
+        <TextInput
+          placeholder="Mobile Number"
+          value={phoneNumber}
+          onChangeText={(text) => setPhoneNumber(text)}
+        />
+      </View>
+
+      <View style={styles.inputBox}>
+        <TextInput
+          placeholder="Date of Birth"
+          value={surname}
+          onChangeText={(text) => setSurname(text)}
+        />
+      </View>
+     
+      <View style={styles.inputBox}>
+        <TextInput
+          placeholder="Company Name"
+          value={surname}
+          onChangeText={(text) => setSurname(text)}
+        />
+      </View>
+
+      <View style={styles.inputBox}>
+        <TextInput
+          placeholder="Company Role"
+          value={surname}
+          onChangeText={(text) => setSurname(text)}
+        />
+      </View>
+
+      <View style={styles.inputBox}>
+        <TextInput
+          placeholder="Address"
+          value={surname}
+          onChangeText={(text) => setSurname(text)}
+        />
+      </View>
+
+      <View style={styles.inputBox}>
+        <TextInput
+          placeholder="Industry"
+          value={surname}
+          onChangeText={(text) => setSurname(text)}
+        />
+      </View>
+      
+      <View style={styles.inputBox}>
+        <TextInput
+          placeholder="Email"
+          value={surname}
+          onChangeText={(text) => setSurname(text)}
+        />
+      </View>
+     
+      <View style={styles.inputBox}>
+        <TextInput
+          placeholder="Linkedin"
+          value={surname}
+          onChangeText={(text) => setSurname(text)}
+        />
+      </View>
+
+      <View style={styles.inputBox}>
+        <TextInput
+          placeholder="Facebook"
+          value={surname}
+          onChangeText={(text) => setSurname(text)}
+        />
+      </View>
+
+      <View style={styles.inputBox}>
+        <TextInput
+          placeholder="Instagram"
+          value={surname}
+          onChangeText={(text) => setSurname(text)}
+        />
+      </View>
+     
+      <View style={styles.inputBox}>
+        <TextInput
+          placeholder="Other Social Media"
+          value={surname}
+          onChangeText={(text) => setSurname(text)}
+        />
+      </View>
+     
+      <View style={styles.inputBox}>
+        <TextInput
+          placeholder="University"
+          value={surname}
+          onChangeText={(text) => setSurname(text)}
+        />
+      </View>
+
+      <View style={styles.inputBox}>
+        <TextInput
+          placeholder="Faculty"
+          value={surname}
+          onChangeText={(text) => setSurname(text)}
+        />
+      </View>
+     
+      <TouchableOpacity style={styles.AddContactButton}>  
+          <Text style={styles.buttonText}>Save</Text> 
+         </TouchableOpacity>
+    </View>
+    </ScrollView>  
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    padding: 30,
+  },
+
+
+  circle: {
+    width:80,
+    height:80,
+    borderRadius: 80/ 2, // Half of width or height to make it circular
+    backgroundColor: 'white', // Background color of the circle
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderStyle: 'solid',
+    borderColor: 'F0F0F0',
+    borderWidth:0.3,
+    marginVertical: 7,
+  },
+  imageIcon: {
+    width: 25, // Adjust the size of the image to fit inside the circle
+    height: 25, // Adjust the size of the image to fit inside the circle
+    //borderRadius: 25, // Half of the adjusted width or height
+    
+  },
+
+  SummaryBox:{
+    width:355,
+    height:135,
+    borderStyle: 'solid',
+    borderColor: 'F0F0F0',
+    borderWidth: 0.3,
+    padding: 20,
+    borderRadius: 15,
+    paddingVertical:5,
+  },
+  AddContactButton:{
+    backgroundColor: 'blue',
+    borderRadius: 15,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    padding: 20,
+    width: 195,
+    height: 55,
+    margin: 20,
+
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+    
+  
+  inputBox: {
+    backgroundColor: 'white',
+    borderStyle: 'solid',
+    borderColor: 'gray',
+    borderWidth: 0.3,
+    padding: 20,
+    borderRadius: 15,
+    width: 355,
+    height: 55,
+    marginVertical: 7,
+
+  },
+});
+
+export default AddContactScreen;
+
